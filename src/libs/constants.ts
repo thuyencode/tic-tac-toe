7a3339cafbaee0b326565e765dba00d7ca2e47ad
thuyencode <thuyencode@proton.me>
2023-12-03 12:17:06 +0700

    Add the Tic Tac Toe grid and overall colors theme

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,2 +1,6 @@
 export const MARKS = ['X', 'O'] as const
 export const SLOTS_COUNT = 9
+export const MARK_ICONS = {
+  X: '<iconify-icon icon="healthicons:x"></iconify-icon>',
+  O: '<iconify-icon icon="healthicons:o"></iconify-icon>'
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans]
+      },
+      height: {
+        screen: '100dvh'
       }
     }
   },
